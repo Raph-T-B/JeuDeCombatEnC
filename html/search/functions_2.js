c0d312dd85d637cccbe40c8248677b56046cd629
Racaumond <raphael.caumond@gmail.com>
2025-01-12 21:06:28 +0100

    ajout doxygen refait

--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -4,7 +4,8 @@ var searchData=
   ['chargerpartie_1',['chargerPartie',['../sae1__02_8h.html#a73096e06dd6817e99979886a99d53566',1,'sae1_02.c']]],
   ['chargescore_2',['chargeScore',['../sae1__02_8h.html#a4660358606958d35747a12ca9cd732a3',1,'sae1_02.c']]],
   ['choixarmej_3',['ChoixArmeJ',['../sae1__02_8h.html#a83563019500c78b7c3c9a48763a96e73',1,'sae1_02.c']]],
-  ['combat_4',['Combat',['../sae1__02_8h.html#a0176cbbe59a3724ac3ee573cf9cee809',1,'sae1_02.c']]],
-  ['copier_5',['copier',['../sae1__02_8h.html#ad1702a0861dbb6b5d23248048ec69b09',1,'sae1_02.c']]],
-  ['creermonstre_6',['creerMonstre',['../sae1__02_8h.html#aa7f442cbd3b0172f00fee79b7b8c4e67',1,'sae1_02.c']]]
+  ['choixbonus_4',['ChoixBonus',['../sae1__02_8h.html#a4c6515fe26d016e082baa8ea4b98bea4',1,'sae1_02.c']]],
+  ['combat_5',['Combat',['../sae1__02_8h.html#a0176cbbe59a3724ac3ee573cf9cee809',1,'sae1_02.c']]],
+  ['copier_6',['copier',['../sae1__02_8h.html#ad1702a0861dbb6b5d23248048ec69b09',1,'sae1_02.c']]],
+  ['creermonstre_7',['creerMonstre',['../sae1__02_8h.html#aa7f442cbd3b0172f00fee79b7b8c4e67',1,'sae1_02.c']]]
 ];
